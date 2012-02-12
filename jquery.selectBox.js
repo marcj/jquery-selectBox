bef17db159a02d5910f6890d5e96c16c96e7d25b
Andrew C. Dvorak <acdvorak@gmail.com>
2012-02-12 14:11:46 -0600

    Added jQuery :obscured plugin

--- jquery.selectBox.js
+++ jquery.selectBox.js
@@ -916,3 +916,64 @@ if(jQuery) (function($) {
 	});
 
 })(jQuery);
+
+/*!
+ * jQuery :obscured
+ *
+ *  @version 1.0.0
+ *
+ *  Copyright 2012, Andrew C. Dvorak
+ *  Released under the MIT, BSD, and GPL Licenses.
+ *  More information: https://github.com/acdvorak/jquery.obscured
+ */
+if(jQuery && !jQuery.expr[':']['obscured']) (function( $, undefined ) {
+
+	var Rect = function(left, top, width, height) {
+		if( ! ( this instanceof Rect ) ) {
+			return new Rect(left, top, width, height);
+		}
+
+		this.left = left;
+		this.top = top;
+		this.right = left + width;
+		this.bottom = top + height;
+
+		this.width = width;
+		this.height = height;
+	};
+
+	Rect.prototype.contains = function(otherRect) {
+		return this.left <= otherRect.left
+			&& this.top <= otherRect.top
+			&& this.right >= otherRect.right
+			&& this.bottom >= otherRect.bottom;
+	};
+
+	$.extend ( $.expr[':'], {
+
+		obscured: function ( obj ) {
+			var $obj = $(obj);
+			var $parent = $obj.offsetParent();
+			var $window = $(window);
+
+			var isHidden = $obj.is(':hidden');
+
+			if( isHidden ) $obj.show();
+
+			var objOffset = $obj.offset();
+
+			var objRect = new Rect(objOffset.left, objOffset.top, $obj.outerWidth(), $obj.outerHeight());
+			var parentRect = new Rect($parent.scrollLeft(), $parent.scrollTop(), $parent.width(), $parent.height());
+			var windowRect = new Rect($window.scrollLeft(), $window.scrollTop(), $window.width(), $window.height());
+
+			if( isHidden ) $obj.hide();
+
+			var parentContains = parentRect.contains(objRect);
+			var windowContains = windowRect.contains(objRect);
+
+			return ! ( parentContains && windowContains );
+		}
+
+	} );
+
+})( jQuery );
